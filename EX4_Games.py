7d37a67d26b392cbd7ea59da6ec10c3dd67913f5
Artur Delpupo <artur.delpupo@gmail.com>
2026-09-07 23:06:19 +0100

    Grafico de vendas globais por ano (mi)

--- EX4_Games.py
+++ EX4_Games.py
@@ -13,11 +13,25 @@ file = os.listdir(path)
 
 data = pd.read_csv(os.path.join(path, file[0]), encoding='iso-8859-1')
 
-print(data.shape)
+#print(data.shape)
 #print(data.head())
-
 #print (data.isnull().sum())
 
 data = data.dropna()
-
+data = data.drop([])
 print(data.describe())
+
+#tamanho da img
+plt.figure(figsize=(13,5))
+
+#titulo da img
+plt.title ('Quantidade de vendas globais por ano (mi)', loc = 'left', fontsize = 14 )
+
+#Gráfico
+sns.barplot(data= data, x = 'Year', y = 'Global', ci = None, estimator=sum) #ci = None tira a barra vertical de desv, estimator = sum faz a soma de cada ano
+
+#legenda
+plt.xlabel ('Quantidades de vendas (mi)')
+
+#plt
+plt.show()
