4e473836a44aa8780c901cc234742abc1d680285
Artur Delpupo <artur.delpupo@gmail.com>
2026-09-07 22:02:36 +0100

    Importanto base de dados PS4_GamesSales.csv, e adicionando encogin com iso 8859-1

---
+++ EX4_Games.py
@@ -0,0 +1,16 @@
+import kagglehub
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+# Download latest version
+path = kagglehub.dataset_download("sidtwr/videogames-sales-dataset")
+
+file = os.listdir(path)
+
+data = pd.read_csv(os.path.join(path, file[0]), encoding='iso-8859-1')
+
+print(data.head())
