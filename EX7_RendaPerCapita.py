47f83c0aabab7be802d2c738427b600a20926643
Artur Delpupo <artur.delpupo@gmail.com>
2026-09-08 17:45:43 +0100

    Adicionando gráficos, combras e ajeitando titulos

--- EX7_RendaPerCapita.py
+++ EX7_RendaPerCapita.py
@@ -11,4 +11,17 @@ data = pd.read_excel('Dados_Pib.xlsx')
 print(data.head())
 print(data.describe)
 
-print (data.groupby(by=['Territorialidades', 'Ano']).mean())
+#print (data.groupby(by=['Territorialidades', 'Ano']).mean())
+
+#Sistema de grids
+fundo = '#f5f5f5'
+
+#Criar sistema de grids
+grid_graf = sns.FacetGrid(data, col='Territorialidades', hue='Territorialidades', col_wrap= 4)
+#Adiciona um grafico de linha a cada mini gráfico
+grid_graf = grid_graf.map(plt.plot, 'Ano', 'PIB per capita')
+#Adicionar sombra para gráficos
+grid_graf = grid_graf.map(plt.fill_between, 'Ano', 'PIB per capita', alpha = 0.2).set_titles('{col_name}') #Set_titles modificou o padrao de titulo, colocando o pais primeiro
+
+#grid_graf = grid_graf.set_titles('{col_name}')
+plt.show()
